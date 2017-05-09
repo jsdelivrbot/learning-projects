71cfeaaac0e263628f6f94f3a52f3814fa3fee0c
Randi Mays <randimays1024@gmail.com>
2017-05-09 14:23:57 -0500

    Adding the redo of the book list applook list application - intro to Redux

---
+++ ReduxSimpleStarter2/src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+	return [
+		{ title: 'The Hunger Games', pages: 303 },
+		{ title: 'Memoirs of a Geisha', pages: 184 },
+		{ title: 'The Memory Keeper\'s Daughter', pages: 483 },
+		{ title: 'The Pursuit of Happyness', pages: 85 }
+	];
+}
+
