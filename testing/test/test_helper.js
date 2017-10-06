aff56b64938d4199f60c8e1f76aa589081f3047a
Randi Mays <randimays1024@gmail.com>
2017-10-06 12:17:28 -0500

    Updating directory

---
+++ testing/test/test_helper.js
@@ -0,0 +1,11 @@
+import jsdom from 'jsdom';
+import jquery from 'jquery';
+
+// Set up testing environment to run like a browser in the command line
+global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
+global.window = global.document.defaultView;
+const $ = jquery(global.window);
+
+// Build 'renderComponent' helper that should render a given React class
+function renderComponent(ComponentClass) {
+}
